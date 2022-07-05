e74c48db1aec83294b9a2ed0f922e86812c20996
jessica mier <jessi.m@live.com.ar>
2022-07-04 21:27:20 -0300

    integracion funciones condionales y ciclos

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
-/*let legajo
+
+const sumar =(numero1,numero2)=> numero1 + numero2
+
+let nota1, nota2, acumulador,promedio
 do{
-    legajo=parseFloat(prompt("ingresa tu legajo"))
-}while(isNaN(legajo))
-alert("ya puedes ingresar")
-console.log(legajo)*/
-
-
-let nota1, nota2, promedio
-
-do{
-    nota1=parseInt(prompt("ingrese nota de parcial de 0 a 10"))
-    nota2=parseInt(prompt("ingrese nota de segundo parcial de 0 a 10"))
+    nota1=parseInt(prompt("Por favor ingrese nota de primer parcial"))
+    nota2=parseInt(prompt("Por favor ingrese nota de segundo parcial"))
+    acumulador= sumar(nota1,nota2)
+    promedio =(acumulador/2)
     
-}while((isNaN(nota1)) || (isNaN(nota2)) || (nota1<0 || nota1>10) || (nota2<0 || nota2>10)){
-    promedio= (nota1 + nota2)/2
-}
-alert( "tu promedio es" +" " + promedio)
+    if(isNaN(nota1)|| isNaN(nota2)){
+        alert("por favor, ingresa una nota valida numerica")
+    }
+    if ((nota1<1 || nota1>10) || (nota2<1 || nota2>10)){
+        alert("Por favor ingrese una nota de 1 a 10")
+    }
+    
+}while((isNaN(nota1) || isNaN(nota2)) || (nota1<1 || nota1>10) || (nota2<1 || nota2>10) )
 
+if(promedio < 4){
+    alert("Debes recursar la materia tu promedio es" +" " + promedio)
+}else if (promedio <=4, promedio<7){
+    alert("Aprobaste la materia pasas a instancia final tu promedio es"+" " + promedio)
+}else if(promedio >=7){
+    alert("felicidades promocionaste la materia tu promedio es" +" "+ promedio,)
+}
